9a80ce88dbf2ea48995ebf9149e406170d079902
anushkamehta31 <only4anushka@gmail.com>
2021-07-13 18:44:12 -0400

    Bottom Navigation Views and fragments created

--- app/src/main/java/com/example/fbuapp/MainActivity.java
+++ app/src/main/java/com/example/fbuapp/MainActivity.java
@@ -1,20 +1,31 @@
 package com.example.fbuapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.fbuapp.databinding.ActivityMainBinding;
+import com.example.fbuapp.fragments.FindGroupFragment;
+import com.example.fbuapp.fragments.GroupsFragment;
+import com.example.fbuapp.fragments.HomeFragment;
+import com.example.fbuapp.fragments.NotificationsFragment;
+import com.example.fbuapp.fragments.ProfileFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnLogout;
     private BottomNavigationView bottomNavigationView;
+    final FragmentManager fragmentManager = getSupportFragmentManager();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,15 +38,35 @@ public class MainActivity extends AppCompatActivity {
 
         bottomNavigationView = binding.bottomNavigation;
 
-        btnLogout = binding.btnLogout;
-        btnLogout.setOnClickListener(new View.OnClickListener() {
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @SuppressLint("NonConstantResourceId")
             @Override
-            public void onClick(View v) {
-                ParseUser.logOut();
-                Intent i = new Intent(getApplicationContext(), LoginActivity.class);
-                startActivity(i);
-                finish();
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment fragment;
+                switch (menuItem.getItemId()) {
+                    case R.id.action_home:
+                        fragment = new HomeFragment();
+                        break;
+                    case R.id.action_groups:
+                        fragment = new GroupsFragment();
+                        break;
+                    case R.id.action_find:
+                        fragment = new FindGroupFragment();
+                        break;
+                    case R.id.action_notifications:
+                        fragment = new NotificationsFragment();
+                        break;
+                    case R.id.action_profile:
+                    default:
+                        fragment = new ProfileFragment();
+                        break;
+                }
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
             }
         });
+        // Set default selection
+        bottomNavigationView.setSelectedItemId(R.id.action_home);
     }
+
 }
