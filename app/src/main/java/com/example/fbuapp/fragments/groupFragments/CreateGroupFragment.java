1d42eaf1bd5b6d0b2949b7b3287e36947ce9a6c2
anushkamehta31 <only4anushka@gmail.com>
2021-07-15 20:01:55 -0400

    Added fields to create group fragment

--- app/src/main/java/com/example/fbuapp/fragments/groupFragments/CreateGroupFragment.java
+++ app/src/main/java/com/example/fbuapp/fragments/groupFragments/CreateGroupFragment.java
@@ -1,21 +1,25 @@
 package com.example.fbuapp.fragments.groupFragments;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
-import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import com.example.fbuapp.R;
+import com.google.android.material.chip.Chip;
+import com.hootsuite.nachos.NachoTextView;
 import com.parse.ParseUser;
-
-import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 import java.util.Map;
@@ -25,6 +29,12 @@ public class CreateGroupFragment extends DialogFragment {
 
     private Map<String, ParseUser> users;
     private List<String> usernames;
+    private NachoTextView acTopics;
+    private Chip cVirtual;
+    private Chip cInPerson;
+    private TextView tvLocation;
+    private int shortAnimationDuration;
+    public static final String TAG = "CreateGroupFragment";
 
     public CreateGroupFragment() {
         // Required empty public constructor
@@ -48,9 +58,53 @@ public class CreateGroupFragment extends DialogFragment {
         super.onViewCreated(view, savedInstanceState);
         getDialog().getWindow().setSoftInputMode(
                 WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+
+        acTopics = view.findViewById(R.id.nTopics);
+        String[] topics = getResources().getStringArray(R.array.topics_array);
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<String>(getContext(), android.R.layout.simple_dropdown_item_1line, topics);
+        acTopics.setAdapter(adapter);
+
+        // Create the tvLocation reference variable and set it to Gone initially
+        tvLocation = view.findViewById(R.id.tvLoc);
+        tvLocation.setVisibility(View.GONE);
+
+        // Get tags to chips
+        cVirtual = view.findViewById(R.id.chipVirtual);
+        cInPerson = view.findViewById(R.id.chipInPerson);
+
+        // Retrieve and cache the system's default "short" animation time.
+        shortAnimationDuration = getResources().getInteger(
+                android.R.integer.config_shortAnimTime);
+
+        // If the In-Person tag is checked, we want to display the Location View
+        cInPerson.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                tvLocation.setVisibility(View.VISIBLE);
+            }
+        });
+
+        // If the In-Person tag is checked, we want to display the Location View
+        cVirtual.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                tvLocation.setVisibility(View.GONE);
+            }
+        });
+
+
     }
 
     public void queryUsers() {
 
+    }
+
+    public void createGroup() {
+        // Check that all fields are filled out
+        // Create new Group Object
+        // Populate group object fields
+        // Save to parse
+        // Return instance of group back to parent fragment
     }
 }
